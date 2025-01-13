2143d0ad5544324b26f83e8cb05f14398d306b32
choi-jian <cordiscja1@kyonggi.ac.kr>
2025-01-13 14:39:11 +0900

    [Bronze II] Title: 나머지, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,8 +1,6 @@
-n=[]
+nums=[]
 for i in range(10):
-  s=int(input())
-  x=s%42
-  n.append(x)
-
-y=set(n)
-print(len(y))
+  n=int(input())
+  if n%42 not in nums:
+    nums.append(n%42)
+print(len(nums))
